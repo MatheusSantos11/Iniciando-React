c021f98b275fe2e43e3876a15256bea85770c082
Matheus Santos <Matheus.soliveira11@hotmail.com>
2025-10-22 09:39:55 -0300

    colocando sistema de login

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,23 +1,26 @@
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import { BrowserRouter, Routes , Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro' 
+import Login from './pages/login/login'
 
-function App()
-{
+function App() {
   return (
     <>
     <BrowserRouter>
     <Navbar />
-    <div className="min-h-[80vh]">
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/home' element={<Home/>}/>
+    <div className='min-h-[80vh]'>
+    <Routes>  
+      <Route path="/" element={<Login />} />
+      <Route path='/home' element={<Home />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Routes>
     </div>
-    <Footer/>
+    <Footer />
     </BrowserRouter>
     </>
+    
   )
 }
 
